62ae8a6ef39654ed4efc31feab0bad1241eb7968
herr-dennis <herr.dennisblacky@hotmail.de>
2025-03-30 22:08:24 +0200

    Aufgabe 7

--- resources/views/testDataView.blade.php
+++ resources/views/testDataView.blade.php
@@ -7,4 +7,8 @@
         <li>{{$item}}</li>
     @endforeach
 
+    @if(\Illuminate\Support\Facades\Session::has("error"))
+        <p style="color: red">{{\Illuminate\Support\Facades\Session::get("error")}}</p>
+    @endif
+
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
+
 Route::get('TestData', function () {
     return app()->make('\App\Http\Controllers\DatenbankController')->getTestData();
 });
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::get('/isloggedin', [App\Http\Controllers\AuthController::class, 'isloggedin'])->name('haslogin');
