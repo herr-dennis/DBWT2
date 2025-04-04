17779628fc2a019a359ab4ae0340b2457335bf0b
herr-dennis <herr.dennisblacky@hotmail.de>
2025-04-04 11:27:40 +0200

    Aufgabe 11

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(DevelopmentDataSeeder::class);
+        //$this->call(DevelopmentDataSeeder::class);
+        $this->call(FactorySeeder::class);
     }
 }
